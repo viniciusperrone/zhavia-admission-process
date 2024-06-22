a1d10f614fc586bf21106df4d7da19a5918485d8
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-06-22 17:24:55 -0300

    chore: express and cors added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,15 @@
-console.log("errro...");
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
