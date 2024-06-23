b8f501b4426c11800445d08757a65ecd05e50e77
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-06-23 11:43:37 -0300

    feat: create UserRepository

--- backend/src/modules/users/infra/typeorm/repositories/UserRepository.ts
+++ backend/src/modules/users/infra/typeorm/repositories/UserRepository.ts
@@ -1,4 +1,38 @@
 import { Repository } from 'typeorm';
 import { dataSource } from '@shared/infra/typeorm';
 
-class UserRepository {}
+import User from '../entities/User';
+
+import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
+import { ICreateUser } from '@modules/users/domain/models/ICreateUser';
+import { IUser } from '@modules/users/domain/models/IUser';
+
+class UserRepository implements IUserRepository {
+  private databaseRepository: Repository<User>;
+
+  constructor() {
+    this.databaseRepository = dataSource.getRepository(User);
+  }
+
+  public async findAll(): Promise<IUser[]> {
+    const users = await this.databaseRepository.find();
+
+    return users;
+  }
+
+  public async create({ name, email, password }: ICreateUser): Promise<IUser> {
+    const user = this.databaseRepository.create({ name, email, password });
+
+    await this.databaseRepository.save(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    await this.databaseRepository.save(user);
+
+    return user;
+  }
+}
+
+export { UserRepository };
