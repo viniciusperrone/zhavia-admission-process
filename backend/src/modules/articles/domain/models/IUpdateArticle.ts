003fbfdce0647ffc90354a9a999a0fd942078c5c
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-06-24 17:21:01 -0300

    fix: improving UpdateArticleService

---
+++ backend/src/modules/articles/domain/models/IUpdateArticle.ts
@@ -0,0 +1,5 @@
+export interface IUpdateArticle {
+  article_id: string;
+  title: string;
+  description: string;
+}
--- backend/src/modules/articles/services/UpdateArticleService.ts
+++ backend/src/modules/articles/services/UpdateArticleService.ts
@@ -2,6 +2,8 @@ import { injectable as Injectable, inject as Inject } from 'tsyringe';
 
 import { IArticleRepository } from '../domain/repositories/IArticleRepository';
 import { IArticle } from '../domain/models/IArticle';
+import { IUpdateArticle } from '../domain/models/IUpdateArticle';
+import AppError from '@shared/errors/AppError';
 
 @Injectable()
 class UpdateArticleService {
@@ -10,8 +12,25 @@ class UpdateArticleService {
     private articleRepository: IArticleRepository,
   ) {}
 
-  public async execute(article: IArticle): Promise<IArticle> {
-    await this.articleRepository.update(article);
+  public async execute({
+    article_id,
+    title,
+    description,
+  }: IUpdateArticle): Promise<IArticle> {
+    const dateUpdated = new Date();
+
+    const articleExist = await this.articleRepository.findById(article_id);
+
+    if (!articleExist) {
+      throw new AppError('Could not find any article with the given id.');
+    }
+
+    const article = await this.articleRepository.update({
+      ...articleExist,
+      title,
+      description,
+      updated_at: dateUpdated,
+    });
 
     return article;
   }
