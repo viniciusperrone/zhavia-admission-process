f0d3cb93d7df31a15f73f498fd4398ba8130154c
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-06-24 17:10:01 -0300

    fix: adjusted CreateArticleService

--- backend/src/modules/articles/services/CreateArticleService.ts
+++ backend/src/modules/articles/services/CreateArticleService.ts
@@ -3,21 +3,36 @@ import { injectable as Injectable, inject as Inject } from 'tsyringe';
 import { IArticleRepository } from '../domain/repositories/IArticleRepository';
 import { ICreateArticle } from '../domain/models/ICreateArticle';
 import { IArticle } from '../domain/models/IArticle';
+import { IUserRepository } from '@modules/users/domain/repositories/IUserRepository';
+import AppError from '@shared/errors/AppError';
+
+type IRequest = Omit<ICreateArticle, 'user'> & {
+  user_id: string;
+};
 
 @Injectable()
 class CreateArticleService {
   constructor(
     @Inject('ArticleRepository')
     private articleRepository: IArticleRepository,
+
+    @Inject('UserRepository')
+    private userRepository: IUserRepository,
   ) {}
 
-  public execute({
-    user,
+  public async execute({
+    user_id,
     title,
     description,
-  }: ICreateArticle): Promise<IArticle> {
+  }: IRequest): Promise<IArticle> {
+    const userExist = await this.userRepository.findById(user_id);
+
+    if (!userExist) {
+      throw new AppError('Could not find any user with the given id.');
+    }
+
     const article = this.articleRepository.create({
-      user,
+      user: userExist,
       title,
       description,
     });
