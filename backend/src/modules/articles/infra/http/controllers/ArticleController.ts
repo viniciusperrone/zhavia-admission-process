50feb3a7c2b05ddc5c3b51ab0169b504721ca8c8
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-06-25 14:40:40 -0300

    fix: improving ArticleController

--- backend/src/modules/articles/infra/http/controllers/ArticleController.ts
+++ backend/src/modules/articles/infra/http/controllers/ArticleController.ts
@@ -8,60 +8,114 @@ import {
   ShowArticleService,
 } from '@modules/articles/services';
 
+import AppError from '@shared/errors/AppError';
+
 class ArticleController {
   public async list(request: Request, response: Response): Promise<Response> {
-    const getArticles = container.resolve(ListArticlesService);
+    try {
+      const getArticles = container.resolve(ListArticlesService);
 
-    const articles = await getArticles.execute();
+      const articles = await getArticles.execute();
 
-    return response.json(articles);
+      return response.json(articles);
+    } catch (error) {
+      console.error(error);
+
+      if (error instanceof AppError) {
+        return response.status(400).json({ message: error.message });
+      }
+
+      return response.status(500).json({ message: 'Internal Server Error' });
+    }
   }
 
   public async get(request: Request, response: Response): Promise<Response> {
-    const { id } = request.params;
+    try {
+      const { id } = request.params;
 
-    const getArticle = container.resolve(ShowArticleService);
+      const getArticle = container.resolve(ShowArticleService);
 
-    const article = await getArticle.execute(id);
+      const article = await getArticle.execute(id);
 
-    return response.json(article);
+      return response.json(article);
+    } catch (error) {
+      console.error(error);
+
+      if (error instanceof AppError) {
+        return response.status(400).json({ message: error.message });
+      }
+
+      return response.status(500).json({ message: 'Internal Server Error' });
+    }
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    const { user_id, title, description } = request.body;
+    try {
+      const { user_id, title, description } = request.body;
 
-    const createArticle = container.resolve(CreateArticleService);
+      const createArticle = container.resolve(CreateArticleService);
 
-    const article = await createArticle.execute({
-      user_id,
-      title,
-      description,
-    });
+      const article = await createArticle.execute({
+        user_id,
+        title,
+        description,
+      });
 
-    return response.json(article);
+      return response.json(article);
+    } catch (error) {
+      console.error(error);
+
+      if (error instanceof AppError) {
+        return response.status(400).json({ message: error.message });
+      }
+
+      return response.status(500).json({ message: 'Internal Server Error' });
+    }
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
-    const { article_id } = request.params;
-    const { title, description } = request.body;
+    try {
+      const { article_id } = request.params;
+      const { title, description } = request.body;
 
-    const updateArticle = container.resolve(UpdateArticleService);
+      const updateArticle = container.resolve(UpdateArticleService);
 
-    const article = await updateArticle.execute({
-      article_id,
-      title,
-      description,
-    });
+      const article = await updateArticle.execute({
+        article_id,
+        title,
+        description,
+      });
 
-    return response.json(article);
+      return response.json(article);
+    } catch (error) {
+      console.error(error);
+
+      if (error instanceof AppError) {
+        return response.status(400).json({ message: error.message });
+      }
+
+      return response.status(500).json({ message: 'Internal Server Error' });
+    }
   }
 
-  public async delete(request: Request, response: Response): Promise<void> {
-    const { article_id } = request.params;
+  public async delete(request: Request, response: Response): Promise<Response> {
+    try {
+      const { article_id } = request.params;
 
-    const deleteArticle = container.resolve(DeleteArticleService);
+      const deleteArticle = container.resolve(DeleteArticleService);
 
-    await deleteArticle.execute(article_id);
+      await deleteArticle.execute(article_id);
+
+      return response.status(201).json([]);
+    } catch (error) {
+      console.error(error);
+
+      if (error instanceof AppError) {
+        return response.status(400).json({ message: error.message });
+      }
+
+      return response.status(500).json({ message: 'Internal Server Error' });
+    }
   }
 }
 
