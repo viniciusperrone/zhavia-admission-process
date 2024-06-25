21a55edabf5e5b65a6765512a2491324b09b7d67
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-06-25 19:33:29 -0300

    feat: add sesstion routes

---
+++ backend/src/modules/users/infra/http/routes/session.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { Joi, Segments, celebrate } from 'celebrate';
+
+import { SessionController } from '../controllers/SessionController';
+
+const sessionRouter = Router();
+const sessionController = new SessionController();
+
+sessionRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionController.create,
+);
+
+export default sessionRouter;
--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/user.routes';
 import articlesRouter from '@modules/articles/infra/http/routes/articles.routes';
+import sessionRouter from '@modules/users/infra/http/routes/session.routes';
 
 const router = Router();
 
 router.use('/user', usersRouter);
 router.use('/article', articlesRouter);
+router.use('/login', sessionRouter);
 
 export default router;
