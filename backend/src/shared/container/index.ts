02f837b37dd547da49b4b52669288a71081ba2af
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-06-29 16:35:14 -0300

    feat: apply ElasticsearchConnection on container

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -5,9 +5,16 @@ import { IUserRepository } from '@modules/users/domain/repositories/IUserReposit
 import { ArticleRepository } from '@modules/articles/infra/typeorm/repositories/ArticleRepository';
 import { IArticleRepository } from '@modules/articles/domain/repositories/IArticleRepository';
 
+import { ElasticsearchConnection } from '@shared/infra/elasticsearch';
+
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 
 container.registerSingleton<IArticleRepository>(
   'ArticleRepository',
   ArticleRepository,
+);
+
+container.registerSingleton<ElasticsearchConnection>(
+  'ElasticsearchConnection',
+  ElasticsearchConnection,
 );
